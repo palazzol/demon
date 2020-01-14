cc7804b2543fa9af6c4a6cfc39410a791a94a5ec
Frank Palazzolo <frank@avoidspikes.com>
2020-01-13 21:07:07 -0500

    Add Bit90 version of moniker (Colecovision cart format)

--- moniker/Z80/build.bat
+++ moniker/Z80/build.bat
@@ -1,3 +1,10 @@
+
 tools\asz80.exe -o -l moniker.asm
 tools\aslink.exe -s moniker.rel
 tools\srec2bin moniker.s19 moniker.bin
+
+tools\asz80.exe -o -l bit90.asm
+tools\aslink.exe -s bit90.rel
+tools\srec2bin -o 8000 bit90.s19 bit90.bin
+
+
