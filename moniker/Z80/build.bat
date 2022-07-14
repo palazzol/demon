b9a3c49dc6d9750a164767fc38874e3549eb7d87
Frank Palazzolo <frank@avoidspikes.com>
2022-07-14 13:26:45 -0400

    Build hex files for all targets

--- moniker/Z80/build.bat
+++ moniker/Z80/build.bat
@@ -2,31 +2,39 @@
 tools\asz80.exe -o -p -s -l moniker.asm
 tools\aslink.exe -m -p -s moniker.rel -u
 tools\srec2bin -a 800 moniker.s19 moniker.bin
+tools\srec_cat.exe moniker.bin -binary -output moniker.hex -Intel -address-length=2 -output_block_size=16
 
 tools\asz80.exe -o -p -s -l bit90.asm
 tools\aslink.exe -m -p -s bit90.rel -u
 tools\srec2bin -o 8000 -a 800 bit90.s19 bit90.bin
+tools\srec_cat.exe bit90.bin -binary -output bit90.hex -Intel -address-length=2 -output_block_size=16
 
 tools\asz80.exe -o -p -s -l sraider.asm
 tools\aslink.exe -m -p -s sraider.rel -u
 tools\srec2bin -a 800 sraider.s19 sraider.bin
+tools\srec_cat.exe sraider.bin -binary -output sraider.hex -Intel -address-length=2 -output_block_size=16
 
 tools\asz80.exe -o -p -s -l sraider2.asm
 tools\aslink.exe -m -p -s sraider2.rel -u
 tools\srec2bin -a 800 sraider2.s19 sraider2.bin
+tools\srec_cat.exe sraider2.bin -binary -output sraider2.hex -Intel -address-length=2 -output_block_size=16
 
 tools\asz80.exe -o -p -s -l gorf.asm
 tools\aslink.exe -m -p -s gorf.rel -u
 tools\srec2bin -a 800 gorf.s19 gorf.bin
+tools\srec_cat.exe gorf.bin -binary -output gorf.hex -Intel -address-length=2 -output_block_size=16
 
 tools\asz80.exe -o -p -s -l astrocade.asm
 tools\aslink.exe -m -p -s astrocade.rel -u
 tools\srec2bin -o 2000 -a 800 astrocade.s19 astrocade.bin
+tools\srec_cat.exe astrocade.bin -binary -output astrocade.hex -Intel -address-length=2 -output_block_size=16
 
 tools\asz80.exe -o -p -s -l refz80.asm
 tools\aslink.exe -m -p -s refz80.rel -u
 tools\srec2bin -a 800 refz80.s19 refz80.bin
+tools\srec_cat.exe refz80.bin -binary -output refz80.hex -Intel -address-length=2 -output_block_size=16
 
 tools\asz80.exe -o -p -s -l startrek.asm
 tools\aslink.exe -m -p -s startrek.rel -u
 tools\srec2bin -a 800 startrek.s19 startrek.bin
+tools\srec_cat.exe startrek.bin -binary -output startrek.hex -Intel -address-length=2 -output_block_size=16
--- moniker/modular/z80ref/build.bat
+++ moniker/modular/z80ref/build.bat
@@ -2,3 +2,4 @@
 ..\tools\asz80.exe -o -p -s -l z80ref.asm
 ..\tools\aslink.exe -m -p -s z80ref.rel -u
 ..\tools\srec2bin -a 800 z80ref.s19 z80ref.bin
+..\tools\srec_cat.exe z80ref.bin -binary -output z80ref.hex -Intel -address-length=2 -output_block_size=16
